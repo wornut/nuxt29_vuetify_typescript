91f0fefce37a745c0cf5fc3d0e1f9a4d8b3de407
kpping <kp@kpping.me>
2019-09-03 14:32:45 +0700

    remove createLocalVue

--- test/VuetifyLogo.spec.ts
+++ test/VuetifyLogo.spec.ts
@@ -1,32 +1,19 @@
-import Vuetify, { Vuetify as IVuetify } from 'vuetify';
-import { mount, createLocalVue } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 import VuetifyLogo from '~/components/VuetifyLogo.vue';
 
-const localVue = createLocalVue();
+const localVue = Vue;
 localVue.use(Vuetify);
 
+const wrapper = mount(VuetifyLogo, { localVue });
+
 describe('VuetifyLogo', () => {
-    let vuetify: IVuetify;
-
-    beforeEach(() => {
-        vuetify = new Vuetify();
-    });
-
     test('is a Vue instance', () => {
-        const wrapper = mount(VuetifyLogo, {
-            localVue,
-            vuetify,
-        });
-
         expect(wrapper.isVueInstance()).toBeTruthy();
     });
 
-    it('should be div', () => {
-        const wrapper = mount(VuetifyLogo, {
-            localVue,
-            vuetify,
-        });
-
+    it('should be compile v-img to div', () => {
         expect(wrapper.is('div')).toBe(true);
     });
 });

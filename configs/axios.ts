ca811b0ab68b00fb92047c4ab88c86abc427a116
kpping <kp@kpping.me>
2019-09-03 19:25:11 +0700

    add retryCondition

--- configs/axios.ts
+++ configs/axios.ts
@@ -1,11 +1,12 @@
 import { AxiosRequestConfig } from 'axios';
-import { IAxiosRetryConfig, exponentialDelay } from 'axios-retry';
+import { IAxiosRetryConfig, exponentialDelay, isNetworkError } from 'axios-retry';
 import { APP_API_URL } from './env';
 
 const axios: AxiosRequestConfig & { retry: IAxiosRetryConfig } = {
     baseURL: APP_API_URL,
     retry: {
         retries: 3,
+        retryCondition: isNetworkError,
         retryDelay: exponentialDelay,
     },
 };

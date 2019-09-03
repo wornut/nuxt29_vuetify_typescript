b46ad2d00fdb4434de722d056733c886e86d2704
kpping <kp@kpping.me>
2019-09-03 19:26:33 +0700

    use store in page

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,21 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 class="pb-5">
-      <h1 class="underline headline">Hello, World</h1>
+      <h1 class="underline headline greeting-text">Hello, {{ ipAddress }}</h1>
     </v-flex>
-    <v-flex xs12>
+    <v-flex xs12 class="pb-5">
       <NormalLogo class="d-inline-block" />
       <VuetifyLogo class="d-inline-block" />
+    </v-flex>
+    <v-flex xs12 class="pb-5">
+      <v-btn class="get-ip-address-button" @click="getIpAddress">Get IP Address</v-btn>
     </v-flex>
   </v-layout>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
+import { ipAddressVuexNamespace } from '~/store/ip_address/const';
 
 @Component({
   components: {
@@ -19,7 +23,13 @@ import { Component, Vue } from 'nuxt-property-decorator';
     VuetifyLogo: () => import('~/components/VuetifyLogo.vue'),
   },
 })
-export default class IndexPage extends Vue {}
+export default class IndexPage extends Vue {
+  @ipAddressVuexNamespace.Getter('myIpAddress')
+  private ipAddress!: string;
+
+  @ipAddressVuexNamespace.Action('getIpAddress')
+  private getIpAddress!: () => void;
+}
 </script>
 
 <style scoped></style>
